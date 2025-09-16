e1110422b5254c0bc2b6102b82e26a1df0bd33c9
the-things-which-matter <igabrigham9@gmail.com>
2025-09-16 19:45:12 +0300

    PriorityQueue.cs fix

--- week02/code/PriorityQueue.cs
+++ week02/code/PriorityQueue.cs
@@ -1,4 +1,8 @@
-﻿public class PriorityQueue
+﻿
+using System;
+using System.Collections.Generic;
+
+public class PriorityQueue
 {
     private List<PriorityItem> _queue = new();
 
@@ -24,14 +28,15 @@
 
         // Find the index of the item with the highest priority to remove
         var highPriorityIndex = 0;
-        for (int index = 1; index < _queue.Count - 1; index++)
+        for (int index = 1; index < _queue.Count; index++)
         {
-            if (_queue[index].Priority >= _queue[highPriorityIndex].Priority)
+            if (_queue[index].Priority > _queue[highPriorityIndex].Priority)
                 highPriorityIndex = index;
         }
 
         // Remove and return the item with the highest priority
         var value = _queue[highPriorityIndex].Value;
+        _queue.RemoveAt(highPriorityIndex);
         return value;
     }
 
